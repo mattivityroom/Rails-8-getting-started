38c8f765d4e95171ef0a0f62c8079ea8c339a2c7
mattivityroom <mattivityroom@gmail.com>
2025-02-17 16:42:42 +0700

    Authentication user

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  allow_unauthenticated_access only: [ :index, :show ]
   before_action :set_product, only: [ :show, :edit, :update, :destroy ]
 
   def index
